93c65f04a3884eae756427b1801e04011bab0eeb
RimshaSaleem <rimshatif8@gmail.com>
2021-09-07 01:59:30 -0700

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 const Account = require('./accounts-model')
+const db = require('../../data/db-config')
 exports.checkAccountPayload = (req, res, next) => {
   const error ={ status: 400 }
   const {name, budget} = req.body;
@@ -31,9 +32,23 @@ else{
 }
 }
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+    const existing = await =db('accounts').where('name', req.body.name.trim()).first()
+    if(existing){
+      next({ status:400, message:'that name is taken' })
+
+    }
+    else{
+      next()
+    }
+
+  }
+  catch (err){
+    next(err)
+
+  }
+ 
   
-  next()
 }
 
 exports.checkAccountId = async (req, res, next) => {
