4ffeb9848c9dd81dc9de3fe71100097b9dbc7e0f
RimshaSaleem <rimshatif8@gmail.com>
2021-09-06 23:56:57 -0700

    added end points

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,73 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR MAGIC 
+  try {
+    res.json('get accont')
+    throw new Error(`ohhhh! !!`)
+    
+
+  }
+  catch(err){
+    next(err)
+
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('get accont by id')
+
+  }
+  catch(err){
+    next(err)
+    
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('post accont')
+
+  }
+  catch(err){
+    next(err)
+    
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('update accont by id')
+
+  }
+  catch(err){
+    next(err)
+    
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('delete accont by id')
+
+  }
+  catch(err){
+    next(err)
+    
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status (err.status || 500).json({
+    message: err.message,
+
+  }) 
 })
 
 module.exports = router;
